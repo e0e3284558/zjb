1ae790e62ebe72fb22f23dfea77cf26cd7d38380
liuzhengjian <liuzhengjian521@qq.com>
2017-08-23 13:13:46 +0800

    增加jqueryValidate前端验证插件

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -821,8 +821,14 @@
     <!-- jqGrid -->
     <script type="text/ecmascript" src="{{ asset('assets/js/plugins/jqGrid/i18n/grid.locale-cn.js') }}"></script>
     <script type="text/ecmascript" src="{{ asset('assets/js/plugins/jqGrid/jquery.jqGrid.min.js') }}"></script>
-
+        
+    <!-- jsTree -->
     <script type="text/ecmascript" src="{{ asset('assets/js/plugins/jsTree/jsTree.min.js') }}"></script>
+    
+    <!-- jqValidate -->
+    <script type="text/ecmascript" src="{{ asset('assets/js/plugins/jqueryValidate/jquery.validate.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/jqueryValidate/additional-methods.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/jqueryValidate/localization/messages_zh.min.js') }}"></script>
 
     <!-- Custom and plugin javascript -->
     <script src="{{ asset('assets/js/inspinia.js') }}" type="text/javascript"></script>
