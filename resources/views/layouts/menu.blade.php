dccb9aaff5429a358eb62c0f5034b46f4b3783b1
liuzhengjian <liuzhengjian521@qq.com>
2017-08-23 13:32:52 +0800

    删除无效图片

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,21 +1,6 @@
 <li class="{{ active_class(if_route('home') || if_query('app_groups',null)) }}">
     <a href="{{ url('home') }}"><i class="fa fa-th-large"></i> <span class="nav-label">控制面板</span></a>
 </li>
-
-<li>
-    <a href="javascript:;"><i class="fa fa-group"></i> <span class="nav-label">用户管理</span> <span
-                class="fa arrow"></span></a>
-    <ul class="nav nav-second-level collapse">
-        <li class="{{ active_class(if_route('users.unit')) }}"><a href="{{ url('users/unit') }}"><i
-                        class="fa fa-angle-right"></i> 单位信息</a></li>
-        <li class="{{ active_class(if_route('users.orgs')) }}"><a href="{{ url('users/orgs') }}"><i
-                        class="fa fa-angle-right"></i> 组织机构</a></li>
-        <li class="{{ active_class(if_route('users.groups')) }}"><a href="{{ url('users/groups') }}"><i
-                        class="fa fa-angle-right"></i> 用户组管理</a></li>
-        <li class="{{ active_class(if_route('users.index')) }}"><a href="{{ url('users/index') }}"><i
-                        class="fa fa-angle-right"></i> 用户列表</a></li>
-    </ul>
-</li>
 <li class="{{ active_class(if_action('App\Http\Controllers\Repair\ClassifyController@index'))}}">
     <a href="javascript:;"><i class="fa fa-group "></i> <span class="nav-label">报修分类</span> <span
                 class="fa arrow"></span></a>
--- resources/views/repair/classify/index.blade.php
+++ resources/views/repair/classify/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
+
+@section('breadcrumb')
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-lg-10">
+            <!-- <h2></h2> -->
+            <ol class="breadcrumb">
+                <li>
+                    <a href="{{ route('home') }}">控制面板</a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">报修管理</a>
+                </li>
+
+                <li class="active">
+                    <strong>报修分类管理</strong>
+                </li>
+            </ol>
+        </div>
+        <div class="col-lg-2">
+        </div>
+    </div>
+    @endsection
 @section('content')
     <div class="wrapper wrapper-content wrapper-content2 animated fadeInRight">
         <div class="row">
