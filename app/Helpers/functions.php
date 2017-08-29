680337fb5969008811e3e8abd71e5a79e84a39af
liuzhengjian <liuzhengjian521@qq.com>
2017-08-29 14:43:56 +0800

    删除暂时无用的js插件

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -212,8 +212,11 @@ if (!function_exists('department_select')) {
 /**
  * 随机返回一个Class样式
  */
-function randomClass(){
-    $arr=['btn-primary','btn-success','btn-info','btn-warning','btn-danger'];
-    $a=array_random($arr);
-    return $a;
+if (!function_exists('randomClass')) {
+    function randomClass()
+    {
+        $arr = ['btn-primary', 'btn-success', 'btn-info', 'btn-warning', 'btn-danger'];
+        $a = array_random($arr);
+        return $a;
+    }
 }
