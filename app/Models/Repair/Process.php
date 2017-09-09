f9dac42c908b2e96b452268d0c57817d9cf80898
ccm <1455394826@qq.com>
2017-09-09 15:17:18 +0800

    接单、维修、评价

--- app/Models/Repair/Process.php
+++ app/Models/Repair/Process.php
@@ -12,6 +12,9 @@ class Process extends Model
     public function user(){
         return $this->belongsTo('App\Models\User\User');
     }
+    public function admin(){
+        return $this->belongsTo('App\Models\User\User','admin_id');
+    }
     public function asset(){
         return $this->belongsTo('App\Models\Asset\Asset');
     }
