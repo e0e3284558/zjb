3460f1b26d0f1c0d4bc499040c5310a6ce289e3a
e0e3284558 <bifei0827@vip.qq.com>
2017-09-18 17:19:33 +0800

    修改场地报修的验证问题

--- resources/views/repair/create_repair/index.blade.php
+++ resources/views/repair/create_repair/index.blade.php
@@ -77,7 +77,7 @@
                                                     <td>{{$v->user_id?$v->user->name:""}}</td>
                                                     <td>{{@get_area($v->area_id)}}</td>
                                                     @if($v->classify && (!$v->asset_id))
-                                                        <td>{{$v->classify->name}}(场地报修)</td>
+                                                        <td>{{$v->classify->name}}</td>
                                                     @else
                                                         <td>{{$v->asset->name}}</td>
                                                     @endif
@@ -145,7 +145,7 @@
                                                         <td>{{$v->user_id?$v->user->name:""}}</td>
                                                         <td>{{@get_area($v->area_id)}}</td>
                                                         @if($v->classify && (!$v->asset_id))
-                                                            <td>{{$v->classify->name}}(场地报修)</td>
+                                                            <td>{{$v->classify->name}}</td>
                                                         @else
                                                             <td>{{$v->asset->name}}</td>
                                                         @endif
@@ -228,7 +228,7 @@
                                                         <td>{{$v->user_id?$v->user->name:""}}</td>
                                                         <td>{{@get_area($v->area_id)}}</td>
                                                         @if($v->classify && (!$v->asset_id))
-                                                            <td>{{$v->classify->name}}(场地报修)</td>
+                                                            <td>{{$v->classify->name}}</td>
                                                         @else
                                                             <td>{{$v->asset->name}}</td>
                                                         @endif
@@ -295,7 +295,7 @@
                                                         <td>{{$v->user_id?$v->user->name:""}}</td>
                                                         <td>{{@get_area($v->area_id)}}</td>
                                                         @if($v->classify && (!$v->asset_id))
-                                                            <td>{{$v->classify->name}}(场地报修)</td>
+                                                            <td>{{$v->classify->name}}</td>
                                                         @else
                                                             <td>{{$v->asset->name}}</td>
                                                         @endif
@@ -398,7 +398,7 @@
                                                         <td>{{$v->user_id?$v->user->name:""}}</td>
                                                         <td>{{@get_area($v->area_id)}}</td>
                                                         @if($v->classify && (!$v->asset_id))
-                                                            <td>{{$v->classify->name}}(场地报修)</td>
+                                                            <td>{{$v->classify->name}}</td>
                                                         @else
                                                             @if($v->asset)
                                                                 <td>{{$v->asset->name}}</td>
