96ea1a832f93b750bee724d13ed196bf48020cfd
ccm <1455394826@qq.com>
2017-09-19 14:24:09 +0800

    修改维修单详情报修项名称

--- resources/views/repair/repair_list/show.blade.php
+++ resources/views/repair/repair_list/show.blade.php
@@ -12,7 +12,7 @@
                 @if($info->other=="0")
                     {{$info->asset->name}}
                 @else
-                    场地报修
+                    {{$info->classify_id?$info->classify->name:""}}
                 @endif
             </td>
             <td><label class="control-label">所在场地</label></td>
