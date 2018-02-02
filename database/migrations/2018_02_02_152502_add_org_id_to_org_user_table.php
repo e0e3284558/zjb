00955f7c6560feea91be55f57cd4eed39afed2eb
e0e3284558 <bifei0827@vip.qq.com>
2018-02-02 15:41:46 +0800

    创建迁移文件

--- database/migrations/2018_02_02_152502_add_org_id_to_org_user_table.php
+++ database/migrations/2018_02_02_152502_add_org_id_to_org_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrgUserTable extends Migration
+class AddOrgIdToOrgUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,6 @@ class CreateOrgUserTable extends Migration
     public function up()
     {
         Schema::table('org_user', function (Blueprint $table) {
-
         });
     }
 
