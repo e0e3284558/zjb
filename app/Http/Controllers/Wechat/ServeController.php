b4333d362e8a8ac71e4060785479334b7743c465
e0e3284558 <bifei0827@vip.qq.com>
2018-02-05 18:05:35 +0800

    微信公众号插入用户union—id

--- app/Http/Controllers/Wechat/ServeController.php
+++ app/Http/Controllers/Wechat/ServeController.php
@@ -2,20 +2,45 @@
 
 namespace App\Http\Controllers\Wechat;
 
-use App\Models\WeChat\Test;
-use Illuminate\Http\Request;
+use App\Models\User\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Log;
 
 class ServeController extends Controller
 {
-    public function serve()
+
+    public function get_unionID($open_id)
     {
+        //公众号获取token
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx9105e296fd5119cf&secret=3e8211e98a09d18c9410823e9f2781cf';
+        $html = file_get_contents($url);
+        $token = (json_decode($html)->access_token);
+
+        //获取用户的UnionID
+        $get_UnionID_url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=' . $token . '&openid='.$open_id.'=zh_CN ';
+        $UnionID_html = file_get_contents($get_UnionID_url);
+        return $UnionID_html;
+    }
+
+
+    public function serve(){
         $app = app('wechat.official_account');
         $app->server->push(function ($message) {
             switch ($message['MsgType']) {
                 case 'event':
-                    return '收到事件消息,open_id:' . $message['FromUserName'];
+                    $user = User::where('g_open_id')->frist();
+                    if ($user->union_id){
+                        if (! $user->g_open_id) {
+                            $user_g = new User;
+                            $user_g->g_open_id = $message['FromUserName'];
+                        }
+                    }else{
+                        $user_g = new User;
+                        $user_g->g_open_id = $message['FromUserName'];
+                        $user_g->union_id=$this->get_unionID($message['FromUserName']);
+                    }
+                    $user_g->save();
+                    return '收到事件消息UnionID' .  $user_g->union_id;
                     break;
                 case 'text':
                     return '收到文字消息';
@@ -24,7 +49,6 @@ class ServeController extends Controller
                     return $message['FromUserName'];
             }
         });
-
         return $app->server->serve();
     }
 }
---
+++ database/migrations/2018_02_05_170623_add_g_open_id_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGOpenIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('g_open_id')->nullable()->comment('微信公众号open_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
