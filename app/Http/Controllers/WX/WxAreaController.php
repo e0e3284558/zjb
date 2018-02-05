23aa3520b71b6ab167cb67c3ca51a2dde38853c6
ccm <1455394826@qq.com>
2018-02-05 16:45:35 +0800

    修改获取场地信息

--- app/Http/Controllers/WX/WxAreaController.php
+++ app/Http/Controllers/WX/WxAreaController.php
@@ -107,7 +107,8 @@ class WxAreaController extends Controller
         $area_info = Area::where("uuid",$request->uuid)->first();
         $area = [
             'area_id' => $area_info->id,
-            'area_name' => get_area($area_info->id)
+            'area_name' => get_area($area_info->id),
+            'org_id' => $area_info->org_id
         ];
 
         return response()->json($area);
