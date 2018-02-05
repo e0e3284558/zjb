4042dbed331be38fe82ec902eca12cf0c71eccf7
e0e3284558 <bifei0827@vip.qq.com>
2018-02-05 16:57:03 +0800

    增加微信公众号token认证

--- app/Http/Controllers/Wechat/ServeController.php
+++ app/Http/Controllers/Wechat/ServeController.php
@@ -12,12 +12,17 @@ class ServeController extends Controller
     public function serve()
     {
         $app = app('wechat.official_account');
-        $test=new Test;
-        $test->comment=$app;
-        $test->save();
-        $app->server->push(function($message){
-
-            return "欢迎关注 overtrue！";
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息,open_id:' . $message['FromUserName'];
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                default:
+                    return $message['FromUserName'];
+            }
         });
 
         return $app->server->serve();
