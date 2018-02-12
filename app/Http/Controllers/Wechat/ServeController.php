3b2ad4794234bab6a3c1bf6ca0a6a4f1814d237f
e0e3284558 <bifei0827@vip.qq.com>
2018-02-12 10:06:17 +0800

    公众号union_id绑定

--- app/Http/Controllers/Wechat/ServeController.php
+++ app/Http/Controllers/Wechat/ServeController.php
@@ -21,8 +21,8 @@ class ServeController extends Controller
         //获取用户的UnionID
         $get_UnionID_url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=' . $token . '&openid=' . $open_id . '=zh_CN ';
         $UnionID_html = file_get_contents($get_UnionID_url);
-        $union_id=json_decode($UnionID_html)->unionid;
-        return $union_id;
+        $user_info = json_decode($UnionID_html);
+        return $user_info;
     }
 
 
@@ -32,22 +32,33 @@ class ServeController extends Controller
      */
     public function is_applet_user($open_id)
     {
-        $union_id=$this->get_unionID($open_id);
-        $service_worker=ServiceWorker::where('union_id',$union_id)->frist();
-        $user=User::where('union_id',$union_id)->frist();
-        if ($service_worker){
-            if (ServiceWorker::where('union_id',$union_id)->update(['g_open_id'=>$open_id])){
+        $user_info = $this->get_unionID($open_id);
+        $union_id = $user_info->unionid;
+        $service_worker = ServiceWorker::where('union_id', $union_id)->frist();
+        $user = User::where('union_id', $union_id)->frist();
+        if ($service_worker) {
+            if (ServiceWorker::where('union_id', $union_id)->update(['g_open_id' => $open_id])) {
                 return '维修人员身份认证绑定成功';
-            }else{
+            } else {
                 return '维修人员身份认证绑定失败';
             }
         }
-        if ($user){
-            if (User::where('union_id',$union_id)->update(['g_open_id'=>$open_id])){
+        if ($user) {
+            if (User::where('union_id', $union_id)->update(['g_open_id' => $open_id])) {
                 return '恭喜您公众号绑定成功';
-            }else{
+            } else {
                 return '抱歉，公众号绑定失败';
             }
+        }
+        $user = new User;
+        $user->name =$user_info->nickname;
+        $user->avatar =$user_info->headimgurl;
+        $user->g_open_id =$user_info->open_id;
+        $user->union_id =$user_info->union_id;
+        if ($user->save()){
+            return '恭喜您，关注成功';
+        }else{
+            return '关注失败，请联系管理员';
         }
     }
 
@@ -58,7 +69,7 @@ class ServeController extends Controller
         $app->server->push(function ($message) {
             switch ($message['MsgType']) {
                 case 'event':
-                    return  $this->is_applet_user($message['FromUserName']);
+                    return $this->is_applet_user($message['FromUserName']);
                     break;
                 case 'text':
                     return '收到文字消息';
