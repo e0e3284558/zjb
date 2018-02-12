222654cb6cae5ba431f8a4a3d1f6f60d018d56fe
e0e3284558 <bifei0827@vip.qq.com>
2018-02-12 10:31:19 +0800

    公众号union_id绑定

--- app/Http/Controllers/Wechat/ServeController.php
+++ app/Http/Controllers/Wechat/ServeController.php
@@ -16,7 +16,7 @@ class ServeController extends Controller
         //公众号获取token
         $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx9105e296fd5119cf&secret=3e8211e98a09d18c9410823e9f2781cf';
         $html = file_get_contents($url);
-        return substr($html,0,10);
+        return substr($html,0,40);
         $token = (json_decode($html)->access_token);
         //获取用户的UnionID
         $get_UnionID_url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=' . $token . '&openid=' . $open_id . '=zh_CN ';
