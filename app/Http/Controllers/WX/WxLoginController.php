2634fc21736009e44796ca12c18744c6193546ef
ccm <1455394826@qq.com>
2018-02-14 16:08:36 +0800

    修改

--- app/Http/Controllers/WX/WxLoginController.php
+++ app/Http/Controllers/WX/WxLoginController.php
@@ -270,10 +270,7 @@ class WxLoginController extends Controller
         $encryptedData = request('encryptedData', '');
         $iv = request('iv', '');
         //根据 code 获取用户 session_key 等信息, 返回用户openid 和 session_key
-//        $userInfo = $this->wxxcx->getLoginInfo($code);
 
-
-//        return $userInfo;
 
 
 
