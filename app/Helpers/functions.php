9218a0d1d61871b0ae4ba8e6d96fb36d1c1eac89
ccm <1455394826@qq.com>
2018-02-14 15:19:43 +0800

    修改维修用户等了登录

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -680,6 +680,27 @@ if (!function_exists('get_asset_name')) {
 }
 
 
+if(!function_exists('httpRequest')){
+    function httpRequest($url, $data = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($data)){
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        if($output === FALSE ){
+            return false;
+        }
+        curl_close($curl);
+        return json_decode($output,JSON_UNESCAPED_UNICODE);
+    }
+}
+
 
 
 
--- app/Http/Controllers/WX/WxLoginController.php
+++ app/Http/Controllers/WX/WxLoginController.php
@@ -270,41 +270,61 @@ class WxLoginController extends Controller
         $encryptedData = request('encryptedData', '');
         $iv = request('iv', '');
         //根据 code 获取用户 session_key 等信息, 返回用户openid 和 session_key
-        $userInfo = $this->wxxcx->getLoginInfo($code);
+//        $userInfo = $this->wxxcx->getLoginInfo($code);
+
+
+
+
+
+        $code2session_url = sprintf('https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code','wxc6cf5e40791e50d3','d229b002be1f04c32ead5acb5167861c',$code);
+        $userInfo = httpRequest($code2session_url);
+        if(!isset($userInfo['session_key'])){
+            return [
+                'code' => 10000,
+                'code' => '获取 session_key 失败',
+            ];
+        }
+//        $this->sessionKey = $userInfo['session_key'];
+        return $userInfo;
+
+
+
+
+
 
 //        $config = [
 //            'app_id' => 'wxc6cf5e40791e50d3',
 //            'secret' => '6bfd990614e4af1327cb8fb71e9a2674',
 //        ];
 
-        //首先判断维修人员是否已经认证
-        $workerInfo = ServiceWorker::where("union_id",$userInfo['unionid'])->first();
-        if($workerInfo){
-            if($workerInfo->openid){
-                //获取解密后的用户信息
-                return $this->wxxcx->getUserInfo($encryptedData, $iv);
-            }else{
-                return $message = [
-                    'code' => 0,
-                    'message' => '对不起，你不是维修人员'
-                ];
-            }
-        }else{
-            //获取解密后的用户信息
-            return $this->wxxcx->getUserInfo($encryptedData, $iv);
-        }
-
-        $judge = ServiceWorker::where("openid",$userInfo['openid'])->first();
-
-        if(!$judge){
-            return $message = [
-                'code' => 0,
-                'message' => '对不起，你不是维修人员'
-            ];
-        }else{
-            //获取解密后的用户信息
-            return $this->wxxcx->getUserInfo($encryptedData, $iv);
-        }
+//        //首先判断维修人员是否已经认证
+//        $workerInfo = ServiceWorker::where("union_id",$userInfo['unionid'])->first();
+//        if($workerInfo){
+//            if($workerInfo->openid){
+//                //获取解密后的用户信息
+//                return $this->wxxcx->getUserInfo($encryptedData, $iv);
+//            }else{
+//                return $message = [
+//                    'code' => 0,
+//                    'message' => '对不起，你不是维修人员'
+//                ];
+//            }
+//        }else{
+//            //获取解密后的用户信息
+//            return $this->wxxcx->getUserInfo($encryptedData, $iv);
+//        }
+//
+//        $judge = ServiceWorker::where("openid",$userInfo['openid'])->first();
+//
+//        if(!$judge){
+//            return $message = [
+//                'code' => 0,
+//                'message' => '对不起，你不是维修人员'
+//            ];
+//        }else{
+//            //获取解密后的用户信息
+//            return $this->wxxcx->getUserInfo($encryptedData, $iv);
+//        }
     }
 
     /**
