c31389b21e520fab04cca7297777ae6f81e4195d
ccm <1455394826@qq.com>
2018-02-24 22:19:09 +0800

    修改登录

--- app/Http/Controllers/WX/WxLoginController.php
+++ app/Http/Controllers/WX/WxLoginController.php
@@ -279,10 +279,10 @@ class WxLoginController extends Controller
 //        $URL = "https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$secret&js_code=$code&grant_type=authorization_code";
 
 
-        $code2session_url = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
+//        $code2session_url = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
 
-        $code2session_url = sprintf($code2session_url,$appid,$secret,$code);
-        $userInfo = $this->httpRequest($code2session_url);
+//        $code2session_url = sprintf($code2session_url,$appid,$secret,$code);
+//        $userInfo = $this->httpRequest($code2session_url);
 
 //        $userInfo = $this->httpRequest($URL);
 //        if(!isset($userInfo['session_key'])){
@@ -292,7 +292,45 @@ class WxLoginController extends Controller
 //            ];
 //        }
 //        $this->sessionKey = $userInfo['session_key'];
-        return $userInfo;
+//        return $userInfo;
+
+
+
+
+        $appid = 'wx4f4bc4dec97d474b';
+        $sessionKey = 'tiihtNczf5v6AKRyjwEUhQ==';
+
+        $encryptedData="CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM
+            QmRzooG2xrDcvSnxIMXFufNstNGTyaGS
+                9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+
+                3hVbJSRgv+4lGOETKUQz6OYStslQ142d
+                NCuabNPGBzlooOmB231qMM85d2/fV6Ch
+                evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6
+                /1Xx1COxFvrc2d7UL/lmHInNlxuacJXw
+                u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn
+                /Hz7saL8xz+W//FRAUid1OksQaQx4CMs
+                8LOddcQhULW4ucetDf96JcR3g0gfRK4P
+                C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB
+                6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns
+                /8wR2SiRS7MNACwTyrGvt9ts8p12PKFd
+                lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV
+                oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG
+                20f0a04COwfneQAGGwd5oa+T8yO5hzuy
+                Db/XcxxmK01EpqOyuxINew==";
+//
+        $iv = 'r7BXXKkLb8qrSNn05n0qiA==';
+
+        $pc = new WXBizDataCrypt($appid, $sessionKey);
+        $errCode = $pc->decryptData($encryptedData, $iv, $data );
+
+        if ($errCode == 0) {
+            print($data . "\n");
+        } else {
+            print($errCode . "\n");
+        }
+
+
+
 
     }
 
