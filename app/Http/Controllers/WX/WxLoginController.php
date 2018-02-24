6def18f07d8679619071bb0edf0bd1a25a711389
ccm <1455394826@qq.com>
2018-02-24 22:47:56 +0800

    修改登录

--- app/Http/Controllers/WX/WxLoginController.php
+++ app/Http/Controllers/WX/WxLoginController.php
@@ -276,7 +276,7 @@ class WxLoginController extends Controller
         $appid = "wxc6cf5e40791e50d3" ;
         $secret = "f462f2ea18595a45235b5c9512a8575f";
 
-        $URL = "https://api.weixin.qq.com/sns/jscode2session?appid=$appid&secret=$secret&js_code=$code&grant_type=authorization_code";
+        $URL = "https://api.weixin.qq.com/sns/jscode2session?appid=".$appid."&secret=".$secret."&js_code=".$code."&grant_type=authorization_code";
 
         $userInfo = $this->httpRequest($URL);
 
