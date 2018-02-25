73d914d27e84cc1830045cf40d730eee2354e894
ccm <1455394826@qq.com>
2018-02-25 09:36:44 +0800

    修改登录

--- config/wxxcx.php
+++ config/wxxcx.php
@@ -4,13 +4,13 @@ return [
 	/**
 	 * 小程序APPID
 	 */
-//    'appid' => 'wxfb71758f0f043c02',
-    'appid' => 'wxc6cf5e40791e50d3',
+    'appid' => 'wxfb71758f0f043c02',
+//    'appid' => 'wxc6cf5e40791e50d3',
     /**
      * 小程序Secret
      */
-//    'secret' => 'c2185e541c2ab1efcd6b6f4bee333acd',
-    'secret' => 'f462f2ea18595a45235b5c9512a8575f',
+    'secret' => 'c2185e541c2ab1efcd6b6f4bee333acd',
+//    'secret' => 'f462f2ea18595a45235b5c9512a8575f',
     /**
      * 小程序登录凭证 code 获取 session_key 和 openid 地址，不需要改动
      */
